9c195cbf24fbc34f24f8bbb79dc1da474908e52b
Shestoy06 <pochta5550@gmail.com>
2025-01-31 02:31:06 +0100

    check with accordion

--- src/routes/_tab-bar/dialogs/index.tsx
+++ src/routes/_tab-bar/dialogs/index.tsx
@@ -1,14 +1,28 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { Button } from "@telegram-apps/telegram-ui";
+import { Accordion, Button } from "@telegram-apps/telegram-ui";
+import { AccordionContent } from "@telegram-apps/telegram-ui/dist/components/Blocks/Accordion/components/AccordionContent/AccordionContent";
+import { AccordionSummary } from "@telegram-apps/telegram-ui/dist/components/Blocks/Accordion/components/AccordionSummary/AccordionSummary";
+import { useState } from "react";
 
 export const Route = createFileRoute("/_tab-bar/dialogs/")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
+  const [expanded, setExpanded] = useState(false);
   return (
-    <div>
-      <Button mode="bezeled">Hello</Button>
+    <div className="pt-10 pr-4 pl-4">
+      <Accordion
+        expanded={expanded}
+        onChange={() => {
+          setExpanded((prev) => !prev);
+        }}
+      >
+        <AccordionSummary className="rounded-xl bg-primary">
+          Hey
+        </AccordionSummary>
+        <AccordionContent>Yo</AccordionContent>
+      </Accordion>
     </div>
   );
 }
