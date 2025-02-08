bf4a4d64048083530ac68272eb47820a68bc1a20
Shestoy06 <pochta5550@gmail.com>
2025-02-08 04:28:11 +0100

    .

--- src/pages/Folders.tsx
+++ src/pages/Folders.tsx
@@ -7,22 +7,42 @@ import DragSensible from "@/ui/DragSensible";
 import { useDragStore } from "@/lib/zustand-store/drag-store";
 import { useState } from "react";
 
+// In that component custom animation is used for the folder height
+// To provide smoothest transition, translate animation is used, as height animation is expensive
+// To make smooth transition of all folders going down/up when some folder is expanded,
+// every folder is positioned in absolute and have a dynamic inset top that is updated on every folder height change
+// Such overhead provide best performance for low end devices
+
+const mockFolders = [
+  {
+    id: 1,
+    name: "Folder 1",
+    height: 0,
+  },
+  {
+    id: 2,
+    name: "Folder 2",
+    height: 0,
+  },
+  {
+    id: 3,
+    name: "Folder 3",
+    height: 0,
+  },
+  {
+    id: 4,
+    name: "Folder 4",
+    height: 0,
+  },
+];
+
 export default function Folders() {
   const { setManageDialogsModalOpen } = useModalStore();
   const { draggableItemType } = useDragStore();
 
   const dropFolderAppear = draggableItemType === "folder";
 
-  const [foldersHeight, setFoldersHeight] = useState([
-    {
-      id: 1,
-      height: 60,
-    },
-    {
-      id: 2,
-      height: 60,
-    },
-  ]);
+  const [foldersHeight, setFoldersHeight] = useState(mockFolders);
 
   const setFolderHeight = (id: number, height: number) => {
     setFoldersHeight((prev) =>
@@ -50,21 +70,25 @@ export default function Folders() {
             dropFolderAppear ? "translate-y-14" : "translate-y-0"
           }`}
         >
-          <div className="absolute top-0 left-0 w-full transition-all duration-200 ease-in-out">
-            <Folder
-              setFolderHeight={(height: number) => setFolderHeight(1, height)}
-            />
-          </div>
-          <div
-            className="absolute top-0 left-0 w-full transition-all duration-300 ease-in-out"
-            style={{
-              transform: `translateY(${getFolderTopInset(1)}px)`,
-            }}
-          >
-            <Folder
-              setFolderHeight={(height: number) => setFolderHeight(2, height)}
-            />
-          </div>
+          {mockFolders.map((folder) => (
+            <div key={folder.id}>
+              <div
+                className="absolute top-0 left-0 w-full transition-all duration-300 ease-in-out"
+                style={{
+                  transform:
+                    folder.id !== 1
+                      ? `translateY(${getFolderTopInset(folder.id - 1)}px)`
+                      : "translateY(0)",
+                }}
+              >
+                <Folder
+                  setFolderHeight={
+                    (height: number) => setFolderHeight(folder.id, height + 16) // 16 is a margin between folders
+                  }
+                />
+              </div>
+            </div>
+          ))}
         </div>
       </div>
 
