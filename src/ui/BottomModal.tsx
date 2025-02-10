5742f6415d5d17c20075804d17577c5470d31015
Shestoy06 <pochta5550@gmail.com>
2025-02-10 13:17:36 +0100

    bottom modal bg

--- src/ui/BottomModal.tsx
+++ src/ui/BottomModal.tsx
@@ -10,7 +10,7 @@ const BottomModal = (props: ModalProps) => {
   return (
     <div
       id={props.id}
-      className={`bg-primary pointer-events-auto p-6 rounded-t-2xl shadow-lg transition-all ease-in-out duration-300 absolute bottom-0 w-full ${
+      className={`bg-secondary pointer-events-auto p-6 rounded-t-2xl shadow-lg transition-all ease-in-out duration-300 absolute bottom-0 w-full ${
         props.isOpen
           ? "translate-y-0 opacity-100"
           : "translate-y-full opacity-0"
