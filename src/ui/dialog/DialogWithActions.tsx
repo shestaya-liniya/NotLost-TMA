4a274564144fe2eb63d438ec0f95c401bf57c7b3
Shestoy06 <pochta5550@gmail.com>
2025-02-11 15:44:22 +0100

    .

--- src/ui/dialog/DialogWithActions.tsx
+++ src/ui/dialog/DialogWithActions.tsx
@@ -61,8 +61,8 @@ export default function DialogWithActions(props: {
             </span>
           </div>
           <div className="flex flex-col">
-            <div className="bg-green-500/10 mt-1 rounded-sm text-[8px] text-green-500 px-1 flex items-center gap-1 py-1 uppercase">
-              dev
+            <div className="bg-green-500/10 mt-1 h-3.5 rounded-sm text-[8px] text-green-500 px-1 flex items-center gap-1 py-1 uppercase">
+              fullstack
             </div>
           </div>
         </div>
