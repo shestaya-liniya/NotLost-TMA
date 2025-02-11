b4264b80d54337c7197759960b7663bcafb5e764
Shestoy06 <pochta5550@gmail.com>
2025-02-11 19:40:05 +0100

    try resolve graph initial chaos

--- src/components/graph/index.tsx
+++ src/components/graph/index.tsx
@@ -1,12 +1,32 @@
+import { useEffect, useRef, useState } from "react";
 import ForceGraph from "./-force-graph";
 import { useJazzProfileContext } from "@/lib/jazz/jazz-provider";
+import { JazzListOfFolders } from "@/lib/jazz/schema";
 
 const Graph = () => {
   const { jazzProfile } = useJazzProfileContext();
+  const [folders, setFolders] = useState<JazzListOfFolders | null>(null);
+  const timeoutRef = useRef<NodeJS.Timeout | null>(null);
 
-  if (!jazzProfile?.folders) return;
+  useEffect(() => {
+    if (!jazzProfile?.folders) return;
 
-  return <ForceGraph data={jazzProfile.folders!} />;
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
+
+    timeoutRef.current = setTimeout(() => {
+      setFolders(jazzProfile.folders);
+    }, 300);
+
+    return () => {
+      if (timeoutRef.current) clearTimeout(timeoutRef.current);
+    };
+  }, [jazzProfile]);
+
+  if (!folders) return null;
+
+  return <ForceGraph data={folders} />;
 };
 
 export default Graph;
