849311038f0b5c17bb6da0fc6d6449357a2e0832
Shestoy06 <pochta5550@gmail.com>
2025-02-14 03:39:47 +0100

    .

--- src/components/EditTagsModal.tsx
+++ src/components/EditTagsModal.tsx
@@ -38,10 +38,10 @@ export default function EditTagsModal() {
 
   useEffect(() => {
     if (editTagsModalOpen) {
-      //document.getElementById("shadow-input")?.focus();
+      inputRef.current?.focus();
+      window.scrollTo(0, 0);
       setTimeout(() => {
-        inputRef.current?.focus();
-        window.scrollTo({ top: 0, behavior: "smooth" });
+        window.scrollTo(0, 0);
       }, 310);
     }
   }, [editTagsModalOpen]);
