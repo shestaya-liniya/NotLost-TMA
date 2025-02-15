3c3181f529191169d885fd1a54dd1d403743c265
Shestoy06 <pochta5550@gmail.com>
2025-02-15 21:13:52 +0100

    change switch ui

--- src/ui/Switch.tsx
+++ src/ui/Switch.tsx
@@ -11,7 +11,7 @@ export default function Switch(prop: {
         checked={prop.checked}
         onChange={(e) => prop.onChange(e.target.checked)}
       />
-      <div className="relative w-11 h-6 bg-primary peer-focus:outline-none peer-focus:ring-0 rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-link/80 after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-link/30 after:border-link/30 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-link"></div>
+      <div className="relative w-11 h-6 bg-primary peer-focus:outline-none peer-focus:ring-0 rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-link/80 after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-link/30 after:border-link/10 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:after:bg-link"></div>
       <span
         className={`ms-3 text-sm font-medium ${prop.checked ? "text-link" : "text-hint"}`}
       >
