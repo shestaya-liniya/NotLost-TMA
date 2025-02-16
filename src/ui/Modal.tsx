d007e4e55cadc71e1d05283a5b4349169497172f
Shestoy06 <pochta5550@gmail.com>
2025-02-16 23:49:41 +0100

    fix modal

--- src/ui/Modal.tsx
+++ src/ui/Modal.tsx
@@ -1,6 +1,7 @@
 import RemoveIcon from "@/assets/icons/remove.svg?react";
 import { useKeyboardState } from "@/helpers/use-keyboard-visible";
 import useViewportSize from "@/helpers/use-viewport-height";
+import { AnimatePresence, motion } from "framer-motion";
 import { useEffect, useState } from "react";
 
 interface ModalProps {
@@ -26,37 +27,51 @@ const Modal = (props: ModalProps) => {
       } else {
         setHeight(viewportSize?.[1] ?? 0);
       }
+    } else {
+      setHeight(null);
     }
   }, [keyboardState]);
 
   return (
-    <div
-      className={`absolute top-0 left-0 w-full z-50 pointer-events-none transition-height duration-300 ease-in-out`}
-      style={{
-        height: height ? height : "100dvh",
-      }}
-    >
-      <div
-        id={props.id}
-        className={`bg-primary pointer-events-auto pt-6 pl-6 pr-6 pb-2 rounded-2xl shadow-lg transition-all ease-in-out absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[90%] duration-300 ${
-          props.isOpen
-            ? "animate-slideUp  "
-            : "translate-y-full animate-slideDown"
-        }`}
-        onClick={(e) => e.stopPropagation()}
-      >
-        <div className="text-2xl font-semibold text-center mb-4">
-          {props.title}
-        </div>
-        {props.children}
-        <div
-          className="absolute top-2 right-2 text-xl font-semibold text-link rounded-full bg-link/20 p-2"
-          onClick={() => props.onClose()}
+    <AnimatePresence>
+      {props.isOpen && (
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.3 }}
         >
-          <RemoveIcon className="w-4 h-4" />
-        </div>
-      </div>
-    </div>
+          <div
+            className={`absolute top-0 left-0 w-full z-50 transition-height duration-300 ease-in-out bg-black/30`}
+            style={{
+              height: height ? height : "100dvh",
+            }}
+            onClick={props.onClose}
+          >
+            <div
+              id={props.id}
+              className={`bg-primary pointer-events-auto pt-6 pl-6 pr-6 pb-2 rounded-2xl shadow-lg transition-all ease-in-out absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[90%] duration-300 ${
+                props.isOpen
+                  ? "animate-slideUp  "
+                  : "translate-y-full animate-slideDown"
+              }`}
+              onClick={(e) => e.stopPropagation()}
+            >
+              <div className="text-2xl font-semibold text-center mb-4">
+                {props.title}
+              </div>
+              {props.children}
+              <div
+                className="absolute top-2 right-2 text-xl font-semibold text-link rounded-full bg-link/20 p-2"
+                onClick={() => props.onClose()}
+              >
+                <RemoveIcon className="w-4 h-4" />
+              </div>
+            </div>
+          </div>
+        </motion.div>
+      )}
+    </AnimatePresence>
   );
 };
 
