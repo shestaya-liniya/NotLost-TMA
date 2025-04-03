cdf064eea9384a550dcda29cc20130abea38ffbe
Shestoy06 <pochta5550@gmail.com>
2025-04-03 09:29:57 +0200

    refactor: :recycle: settings

---
+++ src/ui/settings/SettingsOneLineSection.tsx
@@ -0,0 +1,10 @@
+import { ReactNode } from "react";
+
+export default function SettingsOneLineSection(props: { children: ReactNode }) {
+  const { children } = props;
+  return (
+    <div className="rounded-2xl items-center bg-primary px-6 py-4 w-full font-semibold flex mt-4">
+      {children}
+    </div>
+  );
+}
