533c97d4c81ee0f8dc4b9a776d7e4e5628a83a2b
Shestoy06 <pochta5550@gmail.com>
2025-04-21 16:42:45 +0200

    .

--- src/features/grid-flow/GridFlow.tsx
+++ src/features/grid-flow/GridFlow.tsx
@@ -8,12 +8,13 @@ import {
 
 import "@xyflow/react/dist/style.css";
 import { initNodes, nodeTypes } from "./GridFlowNodes";
-import { useCallback, useEffect, useRef } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import DraggableAvatars from "./DraggableAvatars";
 import { useDragStore } from "@/lib/store/dragStore";
 import { getMiniAppTopInset } from "@/helpers/css/getMiniAppTopInset";
 import SettingsIcon from "@/assets/icons/settings.svg?react";
 import Tappable from "@/ui/Tappable";
+import GridFlowMenu from "./GridFlowMenu";
 
 const NODE_SIZE = 40;
 
@@ -28,6 +29,8 @@ function GridFlow() {
   const { nodes, setNodes, onNodesChange } = useGridFlowNodesState(initNodes);
   const { getIntersectingNodes, screenToFlowPosition } = useReactFlow();
   const reactFlowWrapper = useRef(null);
+
+  const [showMenu, setShowMenu] = useState(false);
 
   const prevPosition = useRef<{ x: number; y: number }>();
   const lastValidPositionRef = useRef<{ x: number; y: number }>();
@@ -188,14 +191,20 @@ function GridFlow() {
         </div>
       </div>
       <Tappable
+        onClick={() => setShowMenu(!showMenu)}
         style={{
           top: -getMiniAppTopInset() / 2,
         }}
-        className="absolute left-1/2 text-sm max-h-[32px] h-[32px] -translate-x-1/2 backdrop-blur-[25px] bg-black/20 rounded-2xl px-3 py-1.5 font-medium flex items-center gap-2"
+        className="absolute left-1/2 text-sm max-h-[32px] h-[32px] -translate-x-1/2 backdrop-blur-[25px] bg-black/20 rounded-2xl px-3 py-1.5 font-medium flex items-center gap-2 border-[1px] border-[#252525]"
       >
         Workspace
-        <SettingsIcon className="h-3 w-3 text-white" />
+        <SettingsIcon
+          className={`h-4 w-4 text-white transition-transform duration-300 ${
+            showMenu ? "rotate-180" : "rotate-0"
+          }`}
+        />
       </Tappable>
+      <GridFlowMenu show={showMenu} setShow={setShowMenu} />
     </div>
   );
 }
---
+++ src/features/grid-flow/GridFlowMenu.tsx
@@ -0,0 +1,69 @@
+import { getMiniAppTopInset } from "@/helpers/css/getMiniAppTopInset";
+import { createPortal } from "react-dom";
+import GrabIcon from "@/assets/icons/cursor-grab.svg?react";
+import { ReactNode } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+
+export default function GridFlowMenu(props: {
+  show: boolean;
+  setShow: (val: boolean) => void;
+}) {
+  if (typeof window === "undefined") return null;
+
+  return createPortal(
+    <AnimatePresence>
+      {props.show && (
+        <>
+          {/* Backdrop */}
+          <motion.div
+            className="absolute top-0 left-0 h-screen w-screen"
+            onClick={() => props.setShow(false)}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          />
+
+          {/* Menu */}
+          <motion.div
+            style={{
+              top: -getMiniAppTopInset() / 2 + 64,
+              boxShadow:
+                "rgba(0, 0, 0, 0.3) 0px 2px 4px 0px, rgba(0, 0, 0, 0.3) 0px 2px 16px 0px",
+            }}
+            initial={{ opacity: 0, y: -8 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -8 }}
+            transition={{ duration: 0.15 }}
+            className="absolute z-20 left-1/2 -translate-x-1/2 bg-[#141414] rounded-xl border-[1px] border-[#252525]"
+          >
+            <MenuItem>
+              <GrabIcon className="w-5 h-5" />
+              <div>Move</div>
+            </MenuItem>
+            <Divider />
+            <MenuItem>
+              <GrabIcon className="w-5 h-5" />
+              <div>Add</div>
+            </MenuItem>
+            <Divider />
+            <MenuItem>
+              <GrabIcon className="w-5 h-5" />
+              <div>Home</div>
+            </MenuItem>
+          </motion.div>
+        </>
+      )}
+    </AnimatePresence>,
+    document.body
+  );
+}
+
+const MenuItem = ({ children }: { children: ReactNode }) => {
+  return (
+    <div className="px-3 py-1.5 flex items-center gap-2 hover:bg-white/5 transition-colors">
+      {children}
+    </div>
+  );
+};
+
+const Divider = () => <div className="bg-[#252525] h-[1px] w-full" />;
