bc48fd48c8c2076cb689cdc25b9f0c31c34f4d79
Shestoy06 <pochta5550@gmail.com>
2025-04-21 14:55:51 +0200

    .

--- src/features/grid-flow/GridFlow.tsx
+++ src/features/grid-flow/GridFlow.tsx
@@ -191,7 +191,7 @@ function GridFlow() {
         style={{
           top: -getMiniAppTopInset() / 2,
         }}
-        className="absolute left-1/2 text-sm -translate-x-1/2 backdrop-blur-[25px] bg-black/20 rounded-2xl px-2 py-1.5 font-medium flex items-center gap-2"
+        className="absolute left-1/2 text-sm max-h-[32px] h-[32px] -translate-x-1/2 backdrop-blur-[25px] bg-black/20 rounded-2xl px-3 py-1.5 font-medium flex items-center gap-2"
       >
         Workspace
         <SettingsIcon className="h-3 w-3 text-white" />
