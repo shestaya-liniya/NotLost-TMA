d7c18ba95fd67a3129e75ccca7cc3954246358b1
Shestoy06 <pochta5550@gmail.com>
2025-04-26 13:28:38 +0200

    fix: :adhesive_bandage: sliding page unmount

--- src/ui/SlidingPage.tsx
+++ src/ui/SlidingPage.tsx
@@ -1,8 +1,8 @@
 import { backButton } from "@telegram-apps/sdk-react";
 import { useEffect } from "react";
 import TelegramWallpaper from "./TelegramWallpaper";
-import { DelayedUnmount } from "./DelayedUnmount";
 import { useModalStore } from "@/lib/store/modalStore";
+import { AnimatePresence, motion } from "framer-motion";
 
 export default function SlidingPage({
   children,
@@ -36,36 +36,36 @@ export default function SlidingPage({
   }, [open]);
 
   return (
-    <div>
-      <div
-        style={{
-          height: "var(--initial-height)",
-        }}
-        className={`absolute top-0 left-0 w-screen bg-black/50 transition-all ease duration-500 ${
-          open ? "opacity-100" : "opacity-0 pointer-events-none"
-        }`}
-      ></div>
-      <div
-        style={{
-          height: "var(--initial-height)",
-        }}
-        className={`absolute top-0 left-0 w-screen bg-primary transition-all ease duration-500 ${
-          open ? "translate-x-0" : "translate-x-full"
-        }`}
-      >
-        {import.meta.env.MODE === "development" && (
-          <div
-            onClick={handleClose}
-            className="top-5 left-5 absolute bg-link/10 rounded-2xl px-2 py-1 text-link z-50"
+    <AnimatePresence>
+      {open && (
+        <div>
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.4, ease: "easeOut" }}
+            className="absolute top-0 left-0 w-screen h-screen bg-black/50"
+          ></motion.div>
+          <motion.div
+            initial={{ x: "100%" }}
+            animate={{ x: 0 }}
+            exit={{ x: "100%" }}
+            transition={{ duration: 0.4, ease: "easeOut" }}
+            className="absolute top-0 left-0 w-screen h-screen bg-primary"
           >
-            Back
-          </div>
-        )}
-        <TelegramWallpaper />
-        <DelayedUnmount mounted={open} delay={500}>
-          {children}
-        </DelayedUnmount>
-      </div>
-    </div>
+            {import.meta.env.MODE === "development" && (
+              <div
+                onClick={handleClose}
+                className="top-5 left-5 absolute bg-link/10 rounded-2xl px-2 py-1 text-link z-50"
+              >
+                Back
+              </div>
+            )}
+            <TelegramWallpaper />
+            {children}
+          </motion.div>
+        </div>
+      )}
+    </AnimatePresence>
   );
 }
