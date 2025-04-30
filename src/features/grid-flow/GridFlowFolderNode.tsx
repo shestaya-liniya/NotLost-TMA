6f09a15ade8f9e61bf680d5085b411561871b8e9
Shestoy06 <pochta5550@gmail.com>
2025-04-30 16:10:49 +0200

    style: :lipstick: folder ui | letter spacing chat

--- src/features/grid-flow/GridFlowFolderNode.tsx
+++ src/features/grid-flow/GridFlowFolderNode.tsx
@@ -4,23 +4,40 @@ import FolderIcon from "@/assets/icons/folder.svg?react";
 export default function GridFlowFolderNode() {
   return (
     <div className="w-38 h-18 bg-primary rounded-xl flex flex-col items-center relative overflow-hidden border-[1px] border-secondary">
-      <div className="text-xs font-medium mt-2  w-full absolute top-0 left-0 pb-1.5 px-4 text-white flex items-center gap-1">
-        <FolderIcon className="w-4 h-4 text-secondary" />
+      <div className="text-xs font-medium mt-2 w-full pb-1.5 px-4 text-white flex items-center gap-1">
+        <FolderIcon className="w-4 h-4 text-hint" />
         <div className="tracking-[1px] text-[10px] text-[#d4d4d4]">Folder</div>
       </div>
-      <div className="flex relative left-2 mt-8">
+      <div className="flex items-center left-3 relative">
         <img
           src={getTelegramAvatarLink("shestaya_liniya")}
           className="h-8 w-8 rounded-full"
         />
         <img
-          src={getTelegramAvatarLink("shestaya_liniya")}
+          style={{
+            boxShadow:
+              "rgba(0, 0, 0, 0.2) 0px 2px 4px 0px, rgba(0, 0, 0, 0.2) 0px 2px 16px 0px",
+          }}
+          src={getTelegramAvatarLink("kopolinaa")}
           className="h-8 w-8 rounded-full relative -left-2"
         />
         <img
-          src={getTelegramAvatarLink("shestaya_liniya")}
+          style={{
+            boxShadow:
+              "rgba(0, 0, 0, 0.2) 0px 2px 4px 0px, rgba(0, 0, 0, 0.2) 0px 2px 16px 0px",
+          }}
+          src={getTelegramAvatarLink("devs_cis")}
           className="h-8 w-8 rounded-full relative -left-4"
         />
+        <div
+          style={{
+            boxShadow:
+              "rgba(0, 0, 0, 0.2) 0px 2px 4px 0px, rgba(0, 0, 0, 0.2) 0px 2px 16px 0px",
+          }}
+          className="h-8 w-8 bg-secondary rounded-full relative -left-6 grid place-content-center"
+        >
+          <div className="text-hint text-[10px]">+9</div>
+        </div>
       </div>
 
       {/* <div className="absolute top-1 right-1 bg-red-500 rounded-full text-xs h-3 w-3 text-[9px] grid place-items-center">
